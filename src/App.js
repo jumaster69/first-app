6afcb8e8a7644631290e3ee3349d5daa417e582a
jumaster69 <129904546+jumaster69@users.noreply.github.com>
2024-09-24 07:52:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { ParentComponent } from './components/ParentComponent';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Proyecto 01 - Componentes en React</h1>
+      </header>
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
